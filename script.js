a336e254406ef7d9955ad3898679c476d21a3db8
Ömer Ulucan <omer.ulucan80@gmail.com>
2024-09-07 23:44:52 -0500

    vector class added

--- script.js
+++ script.js
@@ -6,16 +6,45 @@ const BALLZ = [];
 let LEFT, RIGHT, UP, DOWN;
 let friction = 0.1;
 
+class Vector {
+    constructor(x,y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    add(v) {
+        return new Vector(this.x+v.x, this.y+v.y);
+    }
+
+    subtr(v) {
+        return new Vector(this.x-v.x, this.y-v.y);
+    }
+
+    mag() {
+        return Math.sqrt(this.x**2 + this.y**2);
+    }
+
+    mult(n) {
+        return new Vector(this.x*n, this.y*n);
+    }
+
+    drawVect(start_x, start_y, n, color) {
+        ctx.beginPath();
+        ctx.moveTo(start_x, start_y);
+        ctx.lineTo(start_x + this.x*n, start_y + this.y*n);
+        ctx.strokeStyle = color;
+        ctx.stroke();
+    }
+}
+
 class Ball {
     constructor(x, y, r, color) {
         this.x = x;
         this.y = y;
         this.r = r;
         this.color = color;
-        this.vel_x=0;
-        this.vel_y=0;
-        this.acc_x=0;
-        this.acc_y=0;
+        this.vel = new Vector(0,0);
+        this.acc = new Vector(0,0);
         this.acceleration = 1;
         this.player = false;
         BALLZ.push(this);
@@ -31,17 +60,8 @@ class Ball {
     }
 
     display() {
-        ctx.beginPath();
-        ctx.moveTo(this.x, this.y);
-        ctx.lineTo(this.x + this.acc_x*100, this.y + this.acc_y*100);
-        ctx.strokeStyle = 'green';
-        ctx.stroke();
-
-        ctx.beginPath();
-        ctx.moveTo(this.x, this.y);
-        ctx.lineTo(this.x + this.vel_x*10, this.y + this.vel_y*10);
-        ctx.strokeStyle = 'blue';
-        ctx.stroke();
+        this.vel.drawVect(this.x, this.y, 10, 'green');
+        this.acc.drawVect(this.x, this.y, 100, 'blue');
     }
 }
 
@@ -77,31 +97,29 @@ const keyControl = (b) => {
     });
     
     if(LEFT) {
-        b.acc_x = -b.acceleration;
+        b.acc.x = -b.acceleration;
     }
     if(UP) {
-        b.acc_y = -b.acceleration;
+        b.acc.y = -b.acceleration;
     }
     if(RIGHT) {
-        b.acc_x = b.acceleration;
+        b.acc.x = b.acceleration;
     }
     if(DOWN) {
-        b.acc_y = b.acceleration;
+        b.acc.y = b.acceleration;
     }
     if(!UP && !DOWN) {
-        b.acc_y = 0;
+        b.acc.y = 0;
     }
     if(!LEFT && !RIGHT) {
-        b.acc_x = 0;
+        b.acc.x = 0;
     }
-    b.vel_x += b.acc_x;
-    b.vel_y += b.acc_y;
+    
+    b.vel = b.vel.add(b.acc);
+    b.vel = b.vel.mult(1-friction);
 
-    b.vel_x *= (1-friction);
-    b.vel_y *= (1-friction);
-
-    b.x += b.vel_x;
-    b.y += b.vel_y;
+    b.x += b.vel.x;
+    b.y += b.vel.y;
 }
 
 const mainLoop = () => {
