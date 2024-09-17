bc7cb5d2564ca0b16b422eebd32f30f5e3b46e5d
Ömer Ulucan <omer.ulucan80@gmail.com>
2024-09-16 22:23:02 -0500

    bugs fixed & collision response added

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ class Vector {
 
 class Ball {
     constructor(x, y, r, color) {
-        this.x = new Vector(x, y);
+        this.pos = new Vector(x, y);  // this.x yerine this.pos kullanılmalı
         this.r = r;
         this.color = color;
         this.vel = new Vector(0,0);
@@ -77,10 +77,17 @@ class Ball {
         ctx.strokeStyle = 'black';
         ctx.stroke();
     }
+    reposition() {
+        this.acc = this.acc.unit().mult(this.acceleration);  // b.acceleration yerine this.acceleration
+        this.vel = this.vel.add(this.acc);  // b.acc yerine this.acc
+        this.vel = this.vel.mult(1-friction);
+        this.pos = this.pos.add(this.vel);  // b.vel yerine this.vel
+    }
+     
 }
 
 const keyControl = (b) => {
-    canvas.addEventListener('keydown', (e) => {
+    document.addEventListener('keydown', (e) => {  // canvas yerine document kullanılmalı
         if(e.key === 'ArrowLeft') {
             LEFT=true;
         }
@@ -95,7 +102,7 @@ const keyControl = (b) => {
         }
     });
     
-    canvas.addEventListener('keyup', (e) => {
+    document.addEventListener('keyup', (e) => {  // canvas yerine document kullanılmalı
         if(e.key === 'ArrowLeft') {
             LEFT=false;
         }
@@ -128,12 +135,6 @@ const keyControl = (b) => {
     if(!LEFT && !RIGHT) {
         b.acc.x = 0;
     }
-    
-    b.acc = b.acc.unit().mult(b.acceleration);
-    b.vel = b.vel.add(b.acc);
-    b.vel = b.vel.mult(1-friction);
-
-    b.pos = b.pos.add(b.vel);
 }
 
 const round = (number, precision) => {
@@ -142,21 +143,41 @@ const round = (number, precision) => {
 }
 
 const coll_det_bb = (b1, b2) => {
-    if(b1.r + b2.r >= b2/pos.subtr(b1.pos).mag()) {
+    if(b1.r + b2.r >= b2.pos.subtr(b1.pos).mag()) {  // b2/pos yerine b2.pos kullanılmalı
         return true;
-    }else false;
+    } else {
+        return false;  // else bloğu ve return false eklendi
+    }
 }
 
 const pen_res_bb = (b1, b2) => {
     let dist = b1.pos.subtr(b2.pos);
     let pen_depth = b1.r + b2.r - dist.mag();
-    let pen_res = dist.unit().mult(pen_depth);
+    let pen_res = dist.unit().mult(pen_depth/2);  // pen_depth'i 2'ye bölerek her topu yarı mesafe kadar hareket ettiriyoruz
     b1.pos = b1.pos.add(pen_res);
     b2.pos = b2.pos.add(pen_res.mult(-1));
 }
 
+const coll_res_bb = (b1, b2) => {
+    let normal = b1.pos.subtr(b2.pos).unit();
+    let relVel = b1.vel.subtr(b2.vel);
+    let sepVel = Vector.dot(relVel, normal);
+    let new_sepVel = -sepVel;
+    let sepVelVec = normal.mult(new_sepVel);
+
+    b1.vel = b1.vel.add(sepVelVec);
+    b2.vel = b2.vel.add(sepVelVec.mult(-1));
+}
+
+function momentum_display() {
+    let momentum = Ball1.vel.add(Ball2.vel).mag();
+    ctx.fillStyle = 'black';  // Metin rengi için fillStyle eklendi
+    ctx.font = '16px Arial';  // Metin fontu için font eklendi
+    ctx.fillText(`Momentum: ${round(momentum, 4)}`, 10, 30);  // Koordinatlar değiştirildi
+}
+
 const mainLoop = () => {
-    ctx.clearRect(0,0, canvas.clientWidth, canvas.clientHeight);
+    ctx.clearRect(0,0, canvas.width, canvas.height);  // clientWidth ve clientHeight yerine width ve height kullanılmalı
     BALLZ.forEach((b, index) => {
         b.drawball();
         if(b.player) {
@@ -165,11 +186,13 @@ const mainLoop = () => {
         for(let i=index+1;i<BALLZ.length; i++) {
             if(coll_det_bb(BALLZ[index], BALLZ[i])) {
                 pen_res_bb(BALLZ[index], BALLZ[i]);
+                coll_res_bb(BALLZ[index], BALLZ[i]);
             }
         }
         b.display();
+        b.reposition();
     });
-
+    momentum_display();
     requestAnimationFrame(mainLoop);
 }
 
